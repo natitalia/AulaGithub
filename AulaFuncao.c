79ebc2acb485ffb32df3318badfdd133d6af1154
Vinicius Breda <vinicius_breda_silva@hotmail.com>
2026-08-12 22:13:28 -0300

    Aula de funcoes 12/08/2026

---
+++ AulaFuncao.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	printf("O resultado eh: %d", soma());
+}
+
+int soma()
+{
+	int num1, num2, resultado;
+	num1 = 10;
+	num2 = 5;
+	resultado = num1 + num2;
+	return resultado;
+}
+
