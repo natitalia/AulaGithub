7e038ca8879c783807d0291e90b18034a2bdb2e0
nataliaSilva <nataliapaula695@gmail.com>
2026-09-07 18:18:33 -0300

    atv em c
    Atividade em c

---
+++ Lista8/Atv1.c
@@ -0,0 +1,24 @@
+//1.	POR VALOR - Faça um programa que:
+//e.	se o número A for IMPAR e B for PAR, fazer uma função para exibir o maior número;
+//a.	leia os valores de A, B;
+//b.	se o número A e B for pares, fazer o procedimento soma;
+//c.	se o número A e B forem impares, fazer a função subtração;
+//d.	se o número A for PAR e B for IMPAR, fazer a média de ambos os números;
+
+#include<conio.h>
+#include<stdio.h>
+
+void main()
+{
+	//vars
+	int num1, num2;
+
+	printf("\nOlah, este programa irá ler valores, fazer contas e averiguar se eh impar ou par!");
+	
+	printf("\nInforme um numero: ");
+	scanf("%d", &num1);
+	printf("\nImforme outro numero: ");
+	scanf("%d", &num2);
+	
+	if (num1)
+}
